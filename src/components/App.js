5771fb0ddd8f44a0c0e0b03aa5ca91ce5cad316b
Chester <cheung319@gmail.com>
2022-02-17 23:27:18 -0500

    commit tree 8a4c6fa63211e532825d43416aa5fc6c2d111a5a

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import EyesOnMe from "./EyesOnMe"
+import Keypad from "./Keypad"
+
+function App() {
+    return (
+        <div>
+        Hello
+        </div>
+    )
+}
+
+export default App
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React from "react"
+
+function Keypad (){
+
+    function input(){
+        console.log('Entering password...')
+    }
+    return (
+        <div>
+        <input type= "password" onChange={input}/>
+        </div>
+    )
+}
+
+
+
+export default Keypad
